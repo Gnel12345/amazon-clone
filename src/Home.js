a0e46b25868474360b7335c15b8588ebc95e6d7c
Gnel12345 <gnel1234@yahoo.com>
2021-12-14 15:46:00 -0600

    added header and home page

---
+++ src/Home.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import "./Home.css";
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className = "home">
+               <div className = "home__container">
+                   <img 
+                   className="home__image"
+                   src="https://m.media-amazon.com/images/I/61qoAEwfEsL._SX3000_.jpg" 
+                   alt=""/>
+
+<div className = "home__row">
+<Product title ="The lean startup"
+price={28.99}
+image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+rating={3}
+/>
+<Product
+title="Men's Lightweight Water-Resistant Packable Puffer Jacket"
+price={57.99}
+image="https://m.media-amazon.com/images/I/81M2xZLo2GL._AC_SY200_.jpg"
+rating={5}
+
+
+
+/>
+
+
+
+
+</div>
+<div className="home__row">
+          <Product
+            id="4903850"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={199.99}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="23445930"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="3254354345"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBENRr2oiCZ-XwTbuqBhUKW8_9q17dE6k",
+    authDomain: "clone-18cbc.firebaseapp.com",
+    projectId: "clone-18cbc",
+    storageBucket: "clone-18cbc.appspot.com",
+    messagingSenderId: "898234830971",
+    appId: "1:898234830971:web:aac885ff41b6f4397a92de"
+  };
