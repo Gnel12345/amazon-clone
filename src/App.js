5ecd2def7310b039121ce54f51161ac156171305
Gnel12345 <gnel1234@yahoo.com>
2021-12-15 01:37:42 -0600

    added Checkout page and route to page with React router dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@
 import './App.css';
 import Header from './Header';
 import Home from "./Home";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
     <div className="App">
-     <Header/>
-     <Home/>
+    <Router>
+    <Header/>
+      <Routes>
+        <Route path ="/" element={<Home/>}>
+        
+        </Route>
+        <Route path = "/checkout" element={<Checkout/>}>
+       
+         
+        </Route>
+      </Routes>
+      
+      
+      </Router> 
+    
     </div>
   );
 }
